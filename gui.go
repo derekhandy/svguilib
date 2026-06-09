8dbfe5ca71a31b5d8ea22b0e2bedcdd5ae7a978f
Derek Handy <derekhandy95@gmail.com>
2026-06-09 03:29:48 -0700

    v1.1.4

--- gui.go
+++ gui.go
@@ -1,5 +1,5 @@
 //
-//												svguilib @ v1.1.3
+//												svguilib @ v1.1.4
 //
 //									MIT License, Copyright (c) 2026 Derek Handy
 //							Project can be found at: https://github.com/derekhandy/svguilib
